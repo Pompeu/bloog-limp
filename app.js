7b6bc48150ee50d3e1a47f051fd2b18fd6a3ffb4
Pompeu <itacir@hotmail.com>
2014-10-29 00:51:42 -0200

    atulizado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 
 //'mongodb://localhost/pompeuapi'
 //mongodb://pompeu:552525@ds049130.mongolab.com:49130/pompeuapi
-mongoose.connect('1mongodb://pompeu:552525@ds049130.mongolab.com:49130/pompeuapi', function(err){
+mongoose.connect('mongodb://pompeu:552525@ds049130.mongolab.com:49130/pompeuapi', function(err){
     if(!err)
         console.log("on web");
     else
